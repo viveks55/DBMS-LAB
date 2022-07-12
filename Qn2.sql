ab0ca7e67dad0141dd1e40b3b466300f1216887b
Vivek S <71586511+viveks55@users.noreply.github.com>
2022-07-12 10:14:15 +0530

    Insert values

---
+++ Qn2.sql
@@ -0,0 +1,29 @@
+SQL> insert into Student values(1,'Rahul','Pattom');
+SQL> insert into Student values(2,'Sachin','Ambalam Mukku');
+SQL> insert into Student values(3,'Saurav','Nalanchira');
+SQL> select * from Student;
+
+SQL> insert into Marksheet values(1,78,89,94);
+SQL> insert into Marksheet values(2,54,65,77);
+SQL> insert into Marksheet values(3,23,78,46);
+SQL> select * from Marksheet;
+
+
+
+
+ O/P
+-----
+
+   ROLL_NO SNAME                SADDRESS
+---------- -------------------- --------------------
+         1 Rahul                Pattom
+         2 Sachin               Ambalam Mukku
+         3 Saurav               Nalanchira
+
+
+
+    ROLLNO       SUB1       SUB2       SUB3
+---------- ---------- ---------- ----------
+         1         78         89         94
+         2         54         65         77
+         3         23         78         46
